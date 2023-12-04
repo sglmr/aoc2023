260cb0e62bbe2cf3e4def28ee9673b0e9f3f4a68
stephen gilmore <stephen@gilmore.us>
2023-12-04 05:52:12 -0800

    solved 01

--- 01/main.go
+++ 01/main.go
@@ -1,95 +1,141 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"github.com/sglmr/aoc2023/internal/read"
 	"log"
-	"os"
 	"strconv"
-	"unicode"
+	"strings"
 )
 
-// Token represents a lexical token
-type Token int
+var digitsOne = map[string]int{
+	"1": 1,
+	"2": 2,
+	"3": 3,
+	"4": 4,
+	"5": 5,
+	"6": 6,
+	"7": 7,
+	"8": 8,
+	"9": 9,
+}
 
-var eof = rune(0)
-
-func isDigit(ch rune) bool {
-	return unicode.IsDigit(ch)
+var digitsTwo = map[string]int{
+	"1":     1,
+	"2":     2,
+	"3":     3,
+	"4":     4,
+	"5":     5,
+	"6":     6,
+	"7":     7,
+	"8":     8,
+	"9":     9,
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
 }
 
 func main() {
 
-	// Try the example
-	fmt.Println("Trying example...")
-	fmt.Println("\tWant: ", 142)
-	runFile("./01/example.txt")
-
-	fmt.Println("Trying part 1...")
-	runFile("./01/input.txt")
-}
-
-func runFile(filePath string) {
-	// Read the input file
-	file, err := os.Open(filePath)
+	// Read the example file
+	example, err := read.ReadFile("./01/example.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Defer function to close the file and handle
-	// any outstanding errors associated with opening the file.
-	defer func() {
-		err := file.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
 
-	// Create a scanner to read the file.
-	scanner := bufio.NewScanner(file)
+	// Read the example file
+	exampleTwo, err := read.ReadFile("./01/example2.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// Iterate through the lines of the file.
+	// Read the input file
+	input, err := read.ReadFile("./01/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Run example part 01
+	fmt.Println("example part 1: ", processRows(example, digitsOne))
+
+	// Run input part 01
+	fmt.Println("part 01: ", processRows(input, digitsOne))
+
+	// Run example part 02
+	fmt.Println("example part 2: ", processRows(exampleTwo, digitsTwo))
+
+	// Run input part 02
+	fmt.Println("part 02: ", processRows(input, digitsTwo))
+
+}
+
+func processRows(data []string, digits map[string]int) int {
 	intSum := 0
-	for scanner.Scan() {
-		// Read the line of text
-		line := scanner.Text()
-		// Convert the line of text to an integer
-		i, err := intsFromString(line)
+	for _, row := range data {
+		mi := findMin(row, digits)
+		ma := findMax(row, digits)
+
+		// Concatenate min+max to get the value as a string for the row.
+		stringNum := fmt.Sprintf("%v%v", mi, ma)
+
+		// Convert the string number to an integer
+		i, err := strconv.Atoi(stringNum)
 		if err != nil {
 			log.Fatal(err)
 		}
-		// Sum the integer
+
+		// Add the integers up
 		intSum = intSum + i
 	}
 
-	// Print the final number
-	fmt.Println("\tGot: ", intSum)
+	return intSum
 }
 
-func intsFromString(text string) (int, error) {
-	intStr := ""
-	last := ""
+// Find the maximum digit in a text string
+func findMax(text string, digits map[string]int) int {
 
-	// Iterate through the string of text.
-	for _, r := range text {
-		// If the rune isn't a digit, continue
-		if !unicode.IsDigit(r) {
-			continue
+	// Iterate backwards through the string to perform
+	// a reverse index search on the string
+	for i := len(text); i >= 0; i-- {
+		t := text[i:]
+		// Iterate over every digit to see if it's in the substring
+		for k, v := range digits {
+			l := strings.Index(t, k)
+			// continue if l = -1
+			if l < 0 {
+				continue
+			}
+			// found a match
+			return v
 		}
-		// If it's the first one, save it
-		if len(intStr) == 0 {
-			intStr = string(r)
+	}
+	return -1
+}
+
+// Find the minimum digit in a text string
+func findMin(text string, digits map[string]int) int {
+
+	for i := 0; i <= len(text); i++ {
+		t := text[:i]
+
+		// Iterate though each record in the digits map
+		for k, v := range digits {
+			l := strings.Index(t, k)
+
+			// Try the next value if the value wasn't found
+			if l == -1 {
+				continue
+			}
+			return v
 		}
 
-		last = string(r)
 	}
 
-	// Concatenate the first and last digits
-	intStr = intStr + last
-
-	switch len(intStr) {
-	case 0:
-		return 0, nil
-	default:
-		return strconv.Atoi(intStr)
-	}
+	return -1
 }
